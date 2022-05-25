c256ae91202968fb2cf6067fe309099a8753e2a9
nicolaswilde <wangzhongxi15@mails.ucas.ac.cn>
2022-05-25 10:16:12 +0800

    Update sgemm.cu

--- sgemm.cu
+++ sgemm.cu
@@ -58,8 +58,6 @@ __global__ void mySgemmV1Aligned(
 
     float r_c[TM][TN] = {0.0};
 
-    // one block load A[128, 8] & B[8, 128]
-    // one thread load 4 float32 of A & 4 float32 of B
     int load_a_smem_m = tid >> 1;
     int load_a_smem_k = (tid & 1) << 2;
     int load_b_smem_k = tid >> 5;
@@ -131,8 +129,6 @@ __global__ void mySgemmV2Aligned(
     float r_comp_b[TN];
     float r_c[TM][TN] = {0.0};
 
-    // one block load A[128, 8] & B[8, 128]
-    // one thread load 4 float32 of A & 4 float32 of B
     int load_a_smem_m = tid >> 1;
     int load_a_smem_k = (tid & 1) << 2;
     int load_b_smem_k = tid >> 5;
@@ -160,91 +156,8 @@ __global__ void mySgemmV2Aligned(
 
         #pragma unroll
         for (int tk = 0; tk < BK; tk++) {
-            FLOAT4(r_comp_a[0]) = FLOAT4(s_a[tk][ty * TM    ]);
-            FLOAT4(r_comp_a[4]) = FLOAT4(s_a[tk][ty * TM + 4]);
-            FLOAT4(r_comp_b[0]) = FLOAT4(s_b[tk][tx * TN    ]);
-            FLOAT4(r_comp_b[4]) = FLOAT4(s_b[tk][tx * TN + 4]);
-
-            #pragma unroll
-            for (int tm = 0; tm < TM; tm++) {
-                #pragma unroll
-                for (int tn = 0; tn < TN; tn++) {
-                    r_c[tm][tn] += r_comp_a[tm] * r_comp_b[tn];
-                }
-            }
-        }
-
-        __syncthreads();
-    }
-
-    #pragma unroll
-    for (int i = 0; i < TM; i++) {
-        int store_c_gmem_m = by * BM + ty * TM + i;
-        #pragma unroll
-        for (int j = 0; j < TN; j += 4) {
-            int store_c_gmem_n = bx * BN + tx * TN + j;
-            int store_c_gmem_addr = OFFSET(store_c_gmem_m, store_c_gmem_n, N);
-            FLOAT4(c[store_c_gmem_addr]) = FLOAT4(r_c[i][j]);
-        }
-    }
-}
-
-__global__ void mySgemmV3Aligned(
-    float * __restrict__ a, float * __restrict__ b, float * __restrict__ c,
-    const int M, const int N, const int K) {
-
-    const int BM = 128;
-    const int BN = 128;
-    const int BK = 8;
-    const int TM = 8;
-    const int TN = 8;
-
-    const int bx = blockIdx.x;
-    const int by = blockIdx.y;
-    const int tx = threadIdx.x;
-    const int ty = threadIdx.y;
-    const int tid = ty * blockDim.x + tx;
-
-    __shared__ float s_a[BK][BM];
-    __shared__ float s_b[BK][BN];
-
-    float r_load_a[4];
-    float r_load_b[4];
-    float r_comp_a[TM];
-    float r_comp_b[TN];
-    float r_c[TM][TN] = {0.0};
-
-    // one block load A[128, 8] & B[8, 128]
-    // one thread load 4 float32 of A & 4 float32 of B
-    int load_a_smem_m = tid >> 1;
-    int load_a_smem_k = (tid & 1) << 2;
-    int load_b_smem_k = tid >> 5;
-    int load_b_smem_n = (tid & 31) << 2;
-
-    int load_a_gmem_m = by * BM + load_a_smem_m;
-    int load_b_gmem_n = bx * BN + load_b_smem_n;
-
-    for (int bk = 0; bk < (K + BK - 1) / BK; bk++) {
-
-        int load_a_gmem_k = bk * BK + load_a_smem_k;
-        int load_a_gmem_addr = OFFSET(load_a_gmem_m, load_a_gmem_k, K);
-        int load_b_gmem_k = bk * BK + load_b_smem_k;
-        int load_b_gmem_addr = OFFSET(load_b_gmem_k, load_b_gmem_n, N);
-        FLOAT4(r_load_a[0]) = FLOAT4(a[load_a_gmem_addr]);
-        FLOAT4(r_load_b[0]) = FLOAT4(b[load_b_gmem_addr]);
-
-        s_a[load_a_smem_k    ][load_a_smem_m] = r_load_a[0];
-        s_a[load_a_smem_k + 1][load_a_smem_m] = r_load_a[1];
-        s_a[load_a_smem_k + 2][load_a_smem_m] = r_load_a[2];
-        s_a[load_a_smem_k + 3][load_a_smem_m] = r_load_a[3];
-        FLOAT4(s_b[load_b_smem_k][load_b_smem_n]) = FLOAT4(r_load_b[0]);
-
-        __syncthreads();
-
-        #pragma unroll
-        for (int tk = 0; tk < BK; tk++) {
-            FLOAT4(r_comp_a[0]) = FLOAT4(s_a[tk][ty * TM    ]);
-            FLOAT4(r_comp_a[4]) = FLOAT4(s_a[tk][ty * TM + 4]);
+            FLOAT4(r_comp_a[0]) = FLOAT4(s_a[tk][ty * TM / 2         ]);
+            FLOAT4(r_comp_a[4]) = FLOAT4(s_a[tk][ty * TM / 2 + BM / 2]);
             FLOAT4(r_comp_b[0]) = FLOAT4(s_b[tk][tx * TN / 2         ]);
             FLOAT4(r_comp_b[4]) = FLOAT4(s_b[tk][tx * TN / 2 + BN / 2]);
 
@@ -261,16 +174,24 @@ __global__ void mySgemmV3Aligned(
     }
 
     #pragma unroll
-    for (int i = 0; i < TM; i++) {
-        int store_c_gmem_m = by * BM + ty * TM + i;
+    for (int i = 0; i < TM / 2; i++) {
+        int store_c_gmem_m = by * BM + ty * TM / 2 + i;
         int store_c_gmem_n = bx * BN + tx * TN / 2;
         int store_c_gmem_addr = OFFSET(store_c_gmem_m, store_c_gmem_n, N);
         FLOAT4(c[store_c_gmem_addr]) = FLOAT4(r_c[i][0]);
         FLOAT4(c[store_c_gmem_addr + BN / 2]) = FLOAT4(r_c[i][4]);
     }
+    #pragma unroll
+    for (int i = 0; i < TM / 2; i++) {
+        int store_c_gmem_m = by * BM + BM / 2 + ty * TM / 2 + i;
+        int store_c_gmem_n = bx * BN + tx * TN / 2;
+        int store_c_gmem_addr = OFFSET(store_c_gmem_m, store_c_gmem_n, N);
+        FLOAT4(c[store_c_gmem_addr]) = FLOAT4(r_c[i + TM / 2][0]);
+        FLOAT4(c[store_c_gmem_addr + BN / 2]) = FLOAT4(r_c[i + TM / 2][4]);
+    }
 }
 
-__global__ void mySgemmV4Aligned(
+__global__ void mySgemmV3Aligned(
     float * __restrict__ a, float * __restrict__ b, float * __restrict__ c,
     const int M, const int N, const int K) {
 
@@ -295,8 +216,6 @@ __global__ void mySgemmV4Aligned(
     float r_comp_b[TN];
     float r_c[TM][TN] = {0.0};
 
-    // one block load A[128, 8] & B[8, 128]
-    // one thread load 4 float32 of A & 4 float32 of B
     int load_a_smem_m = tid >> 1;
     int load_a_smem_k = (tid & 1) << 2;
     int load_b_smem_k = tid >> 5;
@@ -334,8 +253,8 @@ __global__ void mySgemmV4Aligned(
 
         #pragma unroll
         for (int tk = 0; tk < BK; tk++) {
-            FLOAT4(r_comp_a[0]) = FLOAT4(s_a[smem_sel][tk][ty * TM    ]);
-            FLOAT4(r_comp_a[4]) = FLOAT4(s_a[smem_sel][tk][ty * TM + 4]);
+            FLOAT4(r_comp_a[0]) = FLOAT4(s_a[smem_sel][tk][ty * TM / 2         ]);
+            FLOAT4(r_comp_a[4]) = FLOAT4(s_a[smem_sel][tk][ty * TM / 2 + BM / 2]);
             FLOAT4(r_comp_b[0]) = FLOAT4(s_b[smem_sel][tk][tx * TN / 2         ]);
             FLOAT4(r_comp_b[4]) = FLOAT4(s_b[smem_sel][tk][tx * TN / 2 + BN / 2]);
 
@@ -359,8 +278,8 @@ __global__ void mySgemmV4Aligned(
 
     #pragma unroll
     for (int tk = 0; tk < BK; tk++) {
-        FLOAT4(r_comp_a[0]) = FLOAT4(s_a[1][tk][ty * TM    ]);
-        FLOAT4(r_comp_a[4]) = FLOAT4(s_a[1][tk][ty * TM + 4]);
+        FLOAT4(r_comp_a[0]) = FLOAT4(s_a[1][tk][ty * TM / 2         ]);
+        FLOAT4(r_comp_a[4]) = FLOAT4(s_a[1][tk][ty * TM / 2 + BM / 2]);
         FLOAT4(r_comp_b[0]) = FLOAT4(s_b[1][tk][tx * TN / 2         ]);
         FLOAT4(r_comp_b[4]) = FLOAT4(s_b[1][tk][tx * TN / 2 + BN / 2]);
 
@@ -374,12 +293,20 @@ __global__ void mySgemmV4Aligned(
     }
 
     #pragma unroll
-    for (int i = 0; i < TM; i++) {
-        int store_c_gmem_m = by * BM + ty * TM + i;
+    for (int i = 0; i < TM / 2; i++) {
+        int store_c_gmem_m = by * BM + ty * TM / 2 + i;
         int store_c_gmem_n = bx * BN + tx * TN / 2;
         int store_c_gmem_addr = OFFSET(store_c_gmem_m, store_c_gmem_n, N);
         FLOAT4(c[store_c_gmem_addr]) = FLOAT4(r_c[i][0]);
         FLOAT4(c[store_c_gmem_addr + BN / 2]) = FLOAT4(r_c[i][4]);
+    }
+    #pragma unroll
+    for (int i = 0; i < TM / 2; i++) {
+        int store_c_gmem_m = by * BM + BM / 2 + ty * TM / 2 + i;
+        int store_c_gmem_n = bx * BN + tx * TN / 2;
+        int store_c_gmem_addr = OFFSET(store_c_gmem_m, store_c_gmem_n, N);
+        FLOAT4(c[store_c_gmem_addr]) = FLOAT4(r_c[i + TM / 2][0]);
+        FLOAT4(c[store_c_gmem_addr + BN / 2]) = FLOAT4(r_c[i + TM / 2][4]);
     }
 }
 
@@ -688,6 +615,7 @@ int main() {
         }
     }
 
+
     {
         printf("\nKernal = mySgemmV2Aligned\n");
 
@@ -737,49 +665,6 @@ int main() {
         const int BM = 128, BN = 128, TM = 8, TN = 8;
         void (*gpuSgemm) (float *, float *, float *, const int, const int, const int) =
             mySgemmV3Aligned;
-
-        {
-            const int M = 512, N = 512, K = 512;
-            dim3 blockDim(BN / TN, BM / TM);
-            dim3 gridDim((N + BN - 1) / BN, (M + BM - 1) / BM);
-            float max_error = testMaxError(gpuSgemm, gridDim, blockDim, M, N, K);
-            printf("Max Error = %f\n", max_error);
-        }
-
-        {
-            const int TESTNUM = 15;
-
-            for (int i = 0; i < TESTNUM; i++) {
-                const int M = M_list[i], N = N_list[i], K = K_list[i];
-
-                dim3 blockDim(BN / TN, BM / TM);
-                dim3 gridDim((N + BN - 1) / BN, (M + BM - 1) / BM);
-
-                double max_sec = 0.0;
-                double min_sec = DBL_MAX;
-                double total_sec = 0.0;
-
-                for (int j = 0; j < outer_repeat; j++) {
-                    double this_sec = testPerformance(gpuSgemm, gridDim, blockDim, M, N, K, inner_repeat);
-                    max_sec = max(max_sec, this_sec);
-                    min_sec = min(min_sec, this_sec);
-                    total_sec += this_sec;
-                }
-
-                double avg_sec = total_sec / outer_repeat;
-                double avg_Gflops = ((double)M) * N * K * 2 / 1024 / 1024 / 1024 / avg_sec;
-
-                printf("M N K = %6d %6d %6d, Time = %12.8lf %12.8lf %12.8lf s, AVG Performance = %10.4lf Gflops\n", M, N, K, min_sec, avg_sec, max_sec, avg_Gflops);
-            }
-        }
-    }
-
-    {
-        printf("\nKernal = mySgemmV4Aligned\n");
-
-        const int BM = 128, BN = 128, TM = 8, TN = 8;
-        void (*gpuSgemm) (float *, float *, float *, const int, const int, const int) =
-            mySgemmV4Aligned;
 
         {
             const int M = 512, N = 512, K = 512;
